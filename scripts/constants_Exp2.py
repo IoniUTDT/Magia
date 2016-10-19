b50884d9d5aa957329c9c405c6b44deb0560e196
Ionatan Perez <ionatan@gmail.com>
2016-10-19 14:10:39 -0300

    Estoy reordenando un poco las cosas y procesando los datos que me habian quedado pendientes antes de la AFA

---
+++ scripts/constants_Exp2.py
@@ -0,0 +1,65 @@
+C_Edades = {'1_1_0':'Menos 5',
+                  '1_1_1':'5 a 12',
+                  '1_1_2':'13 a 17',
+                  '1_1_3':'18 a 24',
+                  '1_1_4':'25 a 40',
+                  '1_1_5':'40 Mas'
+            }
+C_Profesion = {'2_1_0':'Psicologia',
+                  '2_1_1':'Bio/Qui/Geo/Paleo',
+                  '2_1_2':'Prof/Docencia',
+                  '2_1_3':'Medicas',
+                  '2_1_4':'Arq/Diseño',
+                  '2_1_5':'Fisica',
+                  '2_1_6':'Musica',
+                  '2_1_7':'Turismo',
+                  '2_1_8':'Derecho',
+                  '2_1_9':'Mate/Comp/Sistemas',
+                  '2_1_10':'Farm. y Bioq.',
+                  '2_1_11':'Artes Plasticas',
+                  '2_1_12':'Letras',
+                  '2_1_13':'Agro./Vet.',
+                  '2_1_14':'Danzas/Deportes',
+                  '2_1_15':'Ingenierias',
+                  '2_1_16':'Cs. Sociales',
+                  '2_1_17':'Gastronomia',
+                  '2_1_18':'Marketing/Publicidad',
+                  '2_1_19':'Cs. Economicas',
+                  '2_1_20':'OpcionLibre'
+                    }
+
+C_Exactas = ['2_1_1','2_1_5','2_1_9','2_1_15']
+C_Afines = ['2_1_3','2_1_4','2_1_10','2_1_13','2_1_2']
+C_NoAfines = ['2_1_0','2_1_6','2_1_7','2_1_8','2_1_11','2_1_12',
+                    '2_1_14','2_1_16','2_1_17','2_1_18','2_1_19','2_1_20']
+
+C_NivelFormacion = {'1_3_0':'Primaria','1_3_1':'Secundaria','1_3_2':'Terciario',
+                          '1_3_3':'UniversidadCompleta','1_3_4':'UniversidadCurso','1_3_5':'Posgrado'}
+
+C_Mecanismo_Q1 = {'2_1_0':'Calor','2_1_1':'Telekinesis','2_1_2':'Magnetismo','2_1_3':'EnergiaCosmica',
+                     '2_1_4':'FisicaCuantica','2_1_5':'Presion','2_1_6':'Aura Corporal','2_1_7':'Electricidad',
+                     '2_1_8':'No lo se'}
+
+C_Confianza = {'2_2':'Confianza'}
+
+C_Mecanismo_Q2 = {'2_3_0':'Calor','2_3_1':'Telekinesis','2_3_2':'Magnetismo','2_3_3':'EnergiaCosmica',
+                     '2_3_4':'FisicaCuantica','2_3_5':'Presion','2_3_6':'Aura Corporal','2_3_7':'Electricidad',
+                     '2_3_8':'CampoLibre'}
+
+C_RelevanciaMano = {'2_4':'Relevancia Mano'}
+
+C_Mecanismo_Q3 = {'3_1_0':'Calor','3_1_1':'Telekinesis','3_1_2':'Magnetismo','3_1_3':'EnergiaCosmica',
+                     '3_1_4':'FisicaCuantica','3_1_5':'Presion','3_1_6':'Aura Corporal','3_1_7':'Electricidad',
+                     '3_1_8':'No lo se'}
+
+C_Confianza_Q3 = {'3_2':'Confianza'}
+
+C_Truco = {'3_3_0' : 'Si', '3_3_1' : 'No', '3_3_2' : 'No se'}
+
+C_Intereses = {'4_1_1' : 'Bailar', '4_1_2' : 'Cocinar', '4_1_3' : 'Matematica', '4_1_4' : 'Pelis/Series',
+                    '4_1_5' : 'Construir', '4_1_6' : 'Oficina', '4_1_7' : 'Tarot', '4_1_8' : 'Literatura',
+                    '4_1_9' : 'Religion', '4_1_10' : 'Reparar', '4_1_11' : 'Programar' , '4_1_12' : 'Correr',
+                    '4_1_13' : 'Acertijos', '4_1_14' : 'Astrologia', '4_1_15' : 'Deportes Equipo', '4_1_16' : 'Debatir',
+                    '4_1_17' : 'Magia', '4_1_18' : 'Electronica'}
+
+EscalaIntereses = [0,1,2,3,4]

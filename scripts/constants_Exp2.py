904fdceab04de9bc9b88bd090ca476723ae137f6
Ionatan Perez <ionatan@gmail.com>
2016-10-19 21:27:03 -0300

    Hice un codigo re lindo que procesa los datos crudos y devuelve una tabla linda con una columna por pregunta! :)

--- scripts/constants_Exp2.py
+++ scripts/constants_Exp2.py
@@ -5,33 +5,33 @@ C_Edades = {'1_1_0':'Menos 5',
                   '1_1_4':'25 a 40',
                   '1_1_5':'40 Mas'
             }
-C_Profesion = {'2_1_0':'Psicologia',
-                  '2_1_1':'Bio/Qui/Geo/Paleo',
-                  '2_1_2':'Prof/Docencia',
-                  '2_1_3':'Medicas',
-                  '2_1_4':'Arq/Diseño',
-                  '2_1_5':'Fisica',
-                  '2_1_6':'Musica',
-                  '2_1_7':'Turismo',
-                  '2_1_8':'Derecho',
-                  '2_1_9':'Mate/Comp/Sistemas',
-                  '2_1_10':'Farm. y Bioq.',
-                  '2_1_11':'Artes Plasticas',
-                  '2_1_12':'Letras',
-                  '2_1_13':'Agro./Vet.',
-                  '2_1_14':'Danzas/Deportes',
-                  '2_1_15':'Ingenierias',
-                  '2_1_16':'Cs. Sociales',
-                  '2_1_17':'Gastronomia',
-                  '2_1_18':'Marketing/Publicidad',
-                  '2_1_19':'Cs. Economicas',
-                  '2_1_20':'OpcionLibre'
+C_Profesion = {'1_2_0':'Psicologia',
+                  '1_2_1':'Bio/Qui/Geo/Paleo',
+                  '1_2_2':'Prof/Docencia',
+                  '1_2_3':'Medicas',
+                  '1_2_4':'Arq/Diseño',
+                  '1_2_5':'Fisica',
+                  '1_2_6':'Musica',
+                  '1_2_7':'Turismo',
+                  '1_2_8':'Derecho',
+                  '1_2_9':'Mate/Comp/Sistemas',
+                  '1_2_10':'Farm. y Bioq.',
+                  '1_2_11':'Artes Plasticas',
+                  '1_2_12':'Letras',
+                  '1_2_13':'Agro./Vet.',
+                  '1_2_14':'Danzas/Deportes',
+                  '1_2_15':'Ingenierias',
+                  '1_2_16':'Cs. Sociales',
+                  '1_2_17':'Gastronomia',
+                  '1_2_18':'Marketing/Publicidad',
+                  '1_2_19':'Cs. Economicas',
+                  '1_2_20':'OpcionLibre'
                     }
 
-C_Exactas = ['2_1_1','2_1_5','2_1_9','2_1_15']
-C_Afines = ['2_1_3','2_1_4','2_1_10','2_1_13','2_1_2']
-C_NoAfines = ['2_1_0','2_1_6','2_1_7','2_1_8','2_1_11','2_1_12',
-                    '2_1_14','2_1_16','2_1_17','2_1_18','2_1_19','2_1_20']
+C_Exactas = ['1_2_1','1_2_5','1_2_9','1_2_15']
+C_Afines = ['1_2_3','1_2_4','1_2_10','1_2_13','1_2_2']
+C_NoAfines = ['1_2_0','1_2_6','1_2_7','1_2_8','1_2_11','1_2_12',
+                    '1_2_14','1_2_16','1_2_17','1_2_18','1_2_19','1_2_20']
 
 C_NivelFormacion = {'1_3_0':'Primaria','1_3_1':'Secundaria','1_3_2':'Terciario',
                           '1_3_3':'UniversidadCompleta','1_3_4':'UniversidadCurso','1_3_5':'Posgrado'}
@@ -56,10 +56,35 @@ C_Confianza_Q3 = {'3_2':'Confianza'}
 
 C_Truco = {'3_3_0' : 'Si', '3_3_1' : 'No', '3_3_2' : 'No se'}
 
-C_Intereses = {'4_1_1' : 'Bailar', '4_1_2' : 'Cocinar', '4_1_3' : 'Matematica', '4_1_4' : 'Pelis/Series',
+C_Intereses = {'4_1_1' : 'Bailar', '4_1_2' : 'Cocinar', '4_1_3' : 'Matematica', '4_1_4' : 'PelisSeries',
                     '4_1_5' : 'Construir', '4_1_6' : 'Oficina', '4_1_7' : 'Tarot', '4_1_8' : 'Literatura',
                     '4_1_9' : 'Religion', '4_1_10' : 'Reparar', '4_1_11' : 'Programar' , '4_1_12' : 'Correr',
-                    '4_1_13' : 'Acertijos', '4_1_14' : 'Astrologia', '4_1_15' : 'Deportes Equipo', '4_1_16' : 'Debatir',
+                    '4_1_13' : 'Acertijos', '4_1_14' : 'Astrologia', '4_1_15' : 'DeportesEquipo', '4_1_16' : 'Debatir',
                     '4_1_17' : 'Magia', '4_1_18' : 'Electronica'}
 
 EscalaIntereses = [0,1,2,3,4]
+
+preguntasAnidadas = {}
+
+for key, value in C_Intereses.items():
+    dic = {}
+    for escala in EscalaIntereses:
+        dic.update({key+'_'+str(escala):escala})
+    preguntasAnidadas.update({'Interes_' + value:dic})
+
+preguntasUnicas = {'Edad' : C_Edades,
+            'Profesion' : C_Profesion,
+            'Formacion' : C_NivelFormacion,
+            'MecanismoInicial': C_Mecanismo_Q1,
+            'ConfianzaInicial' : C_Confianza,
+            'RelevanciaMano' : C_RelevanciaMano,
+            'MecanismoPost' : C_Mecanismo_Q3,
+            'ConfianzaPost' : C_Confianza_Q3,
+            'HayTruco' : C_Truco
+            }
+
+preguntasUnicas.update (preguntasAnidadas)
+
+preguntasMultiples = {
+            'MecanismoInicialOtros' : C_Mecanismo_Q2
+            }
